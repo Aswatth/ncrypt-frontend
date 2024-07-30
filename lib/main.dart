394f5139b34a6096d4ae1d6d44d3810f09472ab3
Aswatth <aswatth56@gmail.com>
2024-07-30 20:28:51 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,32 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       title: 'Ncrypt',
-      home: MyHomePage(title: 'Ncrypt'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text("Data goes here"),
-      )
+      // home: MyHomePage(title: 'Ncrypt'),
+      home: HomePage()
     );
   }
 }
