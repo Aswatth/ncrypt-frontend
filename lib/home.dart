5ba7a44302186d0597e19f772c929205f919d77e
Aswatth <aswatth56@gmail.com>
2024-08-04 18:09:16 +0530

    Update home.dart
    Removed options

--- lib/home.dart
+++ lib/home.dart
@@ -9,64 +9,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  late List<String> _optionList;
-  late List<Icon> _optionIconList;
-
   @override
   void initState() {
     super.initState();
-    _optionList = ["Login", "Import", "Export"];
-    _optionIconList = [
-      Icon(Icons.login),
-      Icon(Icons.upload),
-      Icon(Icons.download)
-    ];
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Row(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        SizedBox(
-          // color: Colors.deepPurple,
-          width: MediaQuery.sizeOf(context).width * 0.25 > 400
-              ? MediaQuery.sizeOf(context).width * 0.25
-              : 400,
-          // color: Colors.deepPurple,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                width: double.infinity,
-                color: Colors.black,
-                child: Padding(
-                  padding: EdgeInsets.all(50.0),
-                  child: Center(
-                      child: Text(
-                    "Ncrypt",
-                    style: TextStyle(color: Colors.greenAccent, fontSize: 50),
-                  )),
-                ),
-              ),
-              ListView.builder(
-                shrinkWrap: true,
-                  itemCount: _optionList.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(
-                      leading: _optionIconList[index],
-                      title: Text(_optionList[index]),
-                    );
-                  })
-            ],
-          ),
-        ),
-        Expanded(
-            child: Container(
-          child: MasterPasswordPage(),
-        ))
-      ],
-    ));
+      body: Center(
+        child: MasterPasswordPage(),
+      ),
+    );
   }
 }
