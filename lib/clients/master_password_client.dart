139ba1323583cb29dcd7fe09ee234307ce1bd738
Aswatth <aswatth56@gmail.com>
2024-08-12 20:52:20 +0530

    Update master_password_client.dart
    Moved login system client and made it singleton
    Uses JWT token for authorization for setting master_password.

--- lib/clients/master_password_client.dart
+++ lib/clients/master_password_client.dart
@@ -1,19 +1,37 @@
 import 'package:frontend/clients/env_loader.dart';
+import 'package:frontend/clients/system_data_client.dart';
 import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
 class MasterPasswordClient {
+  static final MasterPasswordClient _instance = MasterPasswordClient._internal();
+
+  MasterPasswordClient._internal();
+
+  factory MasterPasswordClient () {
+    return _instance;
+  }
+
   Future<String> setMasterPassword(String password) async {
     var url = Uri.http("localhost:${EnvLoader().PORT}", "/master_password");
 
     String jsonString = convert.jsonEncode({"master_password": password});
-    var response = await http.post(url,body: jsonString);
+
+    var response = await http.post(url,body: jsonString, headers: {"Authorization": "Bearer ${SystemDataClient().jwtToken}"});
 
     if (response.statusCode == 200) {
       return "";
     } else {
-      var json = convert.jsonDecode(response.body) as String;
-      return json;
+      if(response.statusCode == 401) {
+        //regenerate temporary token if older token is expired.
+        await SystemDataClient().login("");
+        print("regenerating token");
+        return setMasterPassword(password);
+      } else {
+        var json = convert.jsonDecode(response.body) as String;
+        return json;
+      }
+
     }
   }
 
@@ -22,20 +40,6 @@ class MasterPasswordClient {
 
     String jsonString = convert.jsonEncode({"master_password": password});
     var response = await http.put(url,body: jsonString);
-
-    if (response.statusCode == 200) {
-      return "";
-    } else {
-      var json = convert.jsonDecode(response.body) as String;
-      return json;
-    }
-  }
-
-  Future<String> login(String password) async {
-    var url = Uri.http("localhost:${EnvLoader().PORT}", "/master_password/validate");
-
-    String jsonString = convert.jsonEncode({"master_password": password, "is_login": "true"});
-    var response = await http.post(url,body: jsonString);
 
     if (response.statusCode == 200) {
       return "";
