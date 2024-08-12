427bf7b6615217bae7dbbf33d2e95a5367a15569
Aswatth <aswatth56@gmail.com>
2024-08-12 20:54:59 +0530

    Update set_password.dart

--- lib/master_password_pages/set_password.dart
+++ lib/master_password_pages/set_password.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/master_password_pages/login.dart';
+import 'package:frontend/custom_snack_bar/custom_snackbar.dart';
+import 'package:frontend/custom_snack_bar/status.dart';
+import 'package:frontend/general_pages/login_page.dart';
 
 import '../clients/master_password_client.dart';
 
@@ -11,82 +13,238 @@ class SetPassword extends StatefulWidget {
 }
 
 class _SetPasswordState extends State<SetPassword> {
-  late MasterPasswordClient client;
+  bool _passwordVisibility = false;
+  bool _confirmPasswordVisibility = false;
+
+  final Map<String, bool> _passwordValidation = Map<String, bool>.from({
+    "Should have at least one digit": false,
+    "Should have at least one uppercase character": false,
+    "Should have at least one special character !, @, #, \$, %, ^, &, *": false,
+    "Should be of length between 8 - 16": false
+  });
+
   final _formKey = GlobalKey<FormState>();
-  final _passwordController = TextEditingController();
-  final _confirmPasswordController = TextEditingController();
+  late TextEditingController _passwordController;
+  late TextEditingController _confirmPasswordController;
 
   @override
   void initState() {
     super.initState();
-    client = MasterPasswordClient();
+    _passwordController = TextEditingController();
+    _confirmPasswordController = TextEditingController();
   }
 
-  void setPassword() {
-    client.setMasterPassword(_passwordController.text).then((value) {
-      if (value.isNotEmpty) {
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(value),
-          ),
-        );
-      } else {
+  void setMasterPassword() {
+    MasterPasswordClient()
+        .setMasterPassword(_passwordController.text)
+        .then((value) {
+      if (value.isEmpty) {
         Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => const LoginPage(),
-          ),
-        );
+            MaterialPageRoute(builder: (BuildContext context) => LoginPage()));
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(CustomSnackBar(status: Status.error, content: value).show());
       }
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 0.0, horizontal: 400.0),
-      child: Form(
-        key: _formKey,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            TextFormField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(hintText: "Enter master password"),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter password';
-                }
-                if (value.length < 8 || value.length > 16) {
-                  return 'Password length must be between 8-16';
-                }
-              },
+    return Scaffold(
+      body: Center(
+        child: FittedBox(
+          child: Form(
+            key: _formKey,
+            child: Card(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Looks like this is your first time.",
+                    style: TextStyle(fontSize: 34),
+                  ),
+                  Text(
+                    "Lets start by setting up the master password",
+                    style: TextStyle(fontSize: 24),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: _passwordValidation.entries.map((m) {
+                        return Row(
+                          children: [
+                            m.value
+                                ? Icon(
+                                    Icons.check,
+                                    color: Colors.green,
+                                  )
+                                : Icon(Icons.close, color: Colors.red),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              m.key,
+                              style: TextStyle(
+                                  color: Colors.white60,
+                                  fontStyle: FontStyle.italic),
+                            )
+                          ],
+                        );
+                      }).toList()),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 300,
+                    child: TextFormField(
+                      controller: _passwordController,
+                      obscureText: !_passwordVisibility,
+                      decoration: InputDecoration(
+                        label: Text(
+                          "Enter master password",
+                        ),
+                        hintMaxLines: 16,
+                        enabled: true,
+                        hintStyle:
+                            TextStyle(color: Colors.white24, fontSize: 14),
+                        hintText: "master password",
+                        suffixIcon: IconButton(
+                          icon: _passwordVisibility
+                              ? Icon(
+                                  Icons.visibility,
+                                  // color: TEXT_COLOR,
+                                )
+                              : Icon(Icons.visibility_off),
+                          onPressed: () {
+                            setState(() {
+                              _passwordVisibility = !_passwordVisibility;
+                            });
+                          },
+                        ),
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          if (RegExp(r'[A-Z]').hasMatch(value)) {
+                            _passwordValidation[
+                                    "Should have at least one uppercase character"] =
+                                true;
+                          } else {
+                            _passwordValidation[
+                                    "Should have at least one uppercase character"] =
+                                false;
+                          }
+                          if (RegExp(r'[0-9]').hasMatch(value)) {
+                            _passwordValidation[
+                                "Should have at least one digit"] = true;
+                          } else {
+                            _passwordValidation[
+                                "Should have at least one digit"] = false;
+                          }
+                          if (RegExp(r'[!@#$%^&*]').hasMatch(value)) {
+                            _passwordValidation[
+                                    "Should have at least one special character !, @, #, \$, %, ^, &, *"] =
+                                true;
+                          } else {
+                            _passwordValidation[
+                                    "Should have at least one special character !, @, #, \$, %, ^, &, *"] =
+                                false;
+                          }
+                          if ((value.length >= 8 && value.length <= 16)) {
+                            _passwordValidation[
+                                "Should be of length between 8 - 16"] = true;
+                          } else {
+                            _passwordValidation[
+                                "Should be of length between 8 - 16"] = false;
+                          }
+                        });
+                      },
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Cannot be empty";
+                        }
+                        if (!RegExp(r'[A-Z]').hasMatch(value)) {
+                          return "Must contain a upper case letter";
+                        }
+                        if (!RegExp(r'[0-9]').hasMatch(value)) {
+                          return "Must contain a digit";
+                        }
+                        if (!RegExp(r'[!@#$%^&*]').hasMatch(value)) {
+                          return "Must contain a special character";
+                        }
+                        if (!(value.length >= 8 && value.length <= 16)) {
+                          return "Must be of length between 8-16";
+                        }
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 300,
+                    child: TextFormField(
+                      controller: _confirmPasswordController,
+                      obscureText: !_confirmPasswordVisibility,
+                      decoration: InputDecoration(
+                        label: Text(
+                          "Confirm master password",
+                        ),
+                        hintMaxLines: 16,
+                        hintStyle:
+                            TextStyle(color: Colors.white24, fontSize: 14),
+                        hintText: "master password",
+                        suffixIcon: IconButton(
+                          icon: _confirmPasswordVisibility
+                              ? Icon(
+                                  Icons.visibility,
+                                )
+                              : Icon(Icons.visibility_off),
+                          onPressed: () {
+                            setState(() {
+                              _confirmPasswordVisibility =
+                                  !_confirmPasswordVisibility;
+                            });
+                          },
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value == null ||
+                            (value != _passwordController.text)) {
+                          return "Password does not match";
+                        }
+                      },
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 300,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          setMasterPassword();
+                        }
+                      },
+                      child: Text("Set master password".toUpperCase()),
+                      style: ElevatedButton.styleFrom(
+                          // backgroundColor: PRIMARY_COLOR,
+                          // foregroundColor: BACKGROUND_COLOR
+                          ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                      "This will be the only password you need to remember! ;)",
+                      style:
+                          TextStyle(fontSize: 14, fontStyle: FontStyle.italic)),
+                ],
+              ),
             ),
-            TextFormField(
-              controller: _confirmPasswordController,
-              obscureText: true,
-              decoration: InputDecoration(hintText: "Confirm master password"),
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Please enter password';
-                }
-                if (value != _passwordController.text) {
-                  return 'Passwords does not match';
-                }
-              },
-            ),
-            TextButton(
-              onPressed: () {
-                if (_formKey.currentState?.validate() == false) {
-                  //do nothing
-                } else {
-                  setPassword();
-                }
-              },
-              child: Text("Set password"),
-            ),
-          ],
+          ),
         ),
       ),
     );
