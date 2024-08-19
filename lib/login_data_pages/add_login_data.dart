6ae86de8fdab449656e844673a39835037141ba5
Aswatth <aswatth56@gmail.com>
2024-08-19 12:04:18 +0530

    Update add_login_data.dart
    Added code to check for at least one account before creating a new login data.

--- lib/login_data_pages/add_login_data.dart
+++ lib/login_data_pages/add_login_data.dart
@@ -23,20 +23,32 @@ class _AddLoginDataState extends State<AddLoginData> {
   final List<Account> _accountList = [];
 
   void addLoginData() {
+    if(_accountList.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(CustomSnackBar(status: Status.error, content: "Should have at least 1 account").show());
+      return;
+    }
     LoginDataClient client = LoginDataClient();
-    client.addLoginData(LoginData(
-        name: _nameController.text,
-        url: _urlController.text,
-        attributes: Attributes(
-            isFavourite: _isFavourite,
-            requireMasterPassword: _requireMasterPassword),
-        accounts: _accountList)).then((value) {
-          if(value is String && value.isEmpty) {
-            ScaffoldMessenger.of(context).showSnackBar(CustomSnackBar(status: Status.success, content: "Successfully added!",).show());
-            Navigator.of(context).pop();
-          } else {
-            ScaffoldMessenger.of(context).showSnackBar(CustomSnackBar(status: Status.error, content: value,).show());
-          }
+    client
+        .addLoginData(LoginData(
+            name: _nameController.text,
+            url: _urlController.text,
+            attributes: Attributes(
+                isFavourite: _isFavourite,
+                requireMasterPassword: _requireMasterPassword),
+            accounts: _accountList))
+        .then((value) {
+      if (value is String && value.isEmpty) {
+        ScaffoldMessenger.of(context).showSnackBar(CustomSnackBar(
+          status: Status.success,
+          content: "Successfully added!",
+        ).show());
+        Navigator.of(context).pop();
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(CustomSnackBar(
+          status: Status.error,
+          content: value,
+        ).show());
+      }
     });
   }
 
@@ -50,165 +62,167 @@ class _AddLoginDataState extends State<AddLoginData> {
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Form(
-                key: _formKey,
-                child: Column(
-        children: [
-          TextFormField(
-            controller: _nameController,
-            decoration: InputDecoration(hintText: "Name", icon: Icon(Icons.person)),
-            maxLength: 16,
-            validator: (value) {
-              if (value == null || value.isEmpty) {
-                return 'Name cannot be empty';
-              }
-            },
-          ),
-          TextFormField(
-            controller: _urlController,
-            decoration: InputDecoration(hintText: "URL", icon: Icon(Icons.link)),
-          ),
-          Row(
+          key: _formKey,
+          child: Column(
             children: [
-              Expanded(
-                child: ListTile(
-                  leading: _isFavourite
-                      ? IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _isFavourite = !_isFavourite;
-                        });
-                      },
-                      icon: Icon(
-                        Icons.favorite,
-                        color: Colors.red,
-                      ))
-                      : IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _isFavourite = !_isFavourite;
-                        });
-                      },
-                      icon: Icon(Icons.heart_broken_outlined)),
-                  title: Text("Favourite"),
+              TextFormField(
+                controller: _nameController,
+                decoration:
+                    InputDecoration(hintText: "Name", icon: Icon(Icons.person)),
+                maxLength: 16,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Name cannot be empty';
+                  }
+                },
+              ),
+              TextFormField(
+                controller: _urlController,
+                decoration:
+                    InputDecoration(hintText: "URL", icon: Icon(Icons.link)),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: ListTile(
+                      leading: _isFavourite
+                          ? IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isFavourite = !_isFavourite;
+                                });
+                              },
+                              icon: Icon(
+                                Icons.favorite,
+                                color: Colors.red,
+                              ))
+                          : IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _isFavourite = !_isFavourite;
+                                });
+                              },
+                              icon: Icon(Icons.heart_broken_outlined)),
+                      title: Text("Favourite"),
+                    ),
+                  ),
+                  Expanded(
+                    child: ListTile(
+                      leading: _requireMasterPassword
+                          ? IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _requireMasterPassword =
+                                      !_requireMasterPassword;
+                                });
+                              },
+                              icon: Icon(
+                                Icons.lock_rounded,
+                                color: Colors.red,
+                              ))
+                          : IconButton(
+                              onPressed: () {
+                                setState(() {
+                                  _requireMasterPassword =
+                                      !_requireMasterPassword;
+                                });
+                              },
+                              icon: Icon(Icons.key_off)),
+                      title: Text("Require master password"),
+                    ),
+                  ),
+                ],
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      _accountList.add(Account.empty());
+                    });
+                  },
+                  child: Text("Add account"),
                 ),
               ),
-              Expanded(
-                child: ListTile(
-                  leading: _requireMasterPassword
-                      ? IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _requireMasterPassword = !_requireMasterPassword;
-                        });
-                      },
-                      icon: Icon(
-                        Icons.lock_rounded,
-                        color: Colors.red,
-                      ))
-                      : IconButton(
-                      onPressed: () {
-                        setState(() {
-                          _requireMasterPassword = !_requireMasterPassword;
-                        });
-                      },
-                      icon: Icon(Icons.key_off)),
-                  title: Text("Require master password"),
-                ),
+              Flexible(
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: _accountList.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ListTile(
+                                  leading: Icon(Icons.person),
+                                  title: TextFormField(
+                                    initialValue: _accountList[index].username,
+                                    decoration: InputDecoration(
+                                      hintText: "Username",
+                                    ),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _accountList[index].username = value;
+                                      });
+                                    },
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Username cannot be empty';
+                                      }
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: ListTile(
+                                  leading: Icon(Icons.password),
+                                  title: TextFormField(
+                                    initialValue: _accountList[index].password,
+                                    obscureText: true,
+                                    decoration:
+                                        InputDecoration(hintText: "Password"),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        _accountList[index].password = value;
+                                      });
+                                    },
+                                    validator: (value) {
+                                      if (value == null || value.isEmpty) {
+                                        return 'Password cannot be empty';
+                                      }
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ),
+                            IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    _accountList.removeAt(index);
+                                  });
+                                },
+                                icon: Icon(Icons.delete))
+                          ],
+                        ),
+                      );
+                    }),
               ),
+              TextButton(
+                  onPressed: () {
+                    if (_formKey.currentState!.validate()) {
+                      addLoginData();
+                    }
+                  },
+                  child: Text("Add"))
             ],
           ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: TextButton(
-              onPressed: () {
-                setState(() {
-                  _accountList.add(Account.empty());
-                });
-              },
-              child: Text("Add account"),
-            ),
-          ),
-          Flexible(
-            child: ListView.builder(
-                // shrinkWrap: true,
-                itemCount: _accountList.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ListTile(
-                              leading: Icon(Icons.person),
-                              title: TextFormField(
-                                initialValue: _accountList[index].username,
-                                decoration: InputDecoration(
-                                  hintText: "Username",
-                                ),
-                                onChanged: (value) {
-                                  setState(() {
-                                    _accountList[index].username = value;
-                                  });
-                                },
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Username cannot be empty';
-                                  }
-                                },
-                              ),
-                            ),
-                          ),
-                        ),
-                        Expanded(
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ListTile(
-                              leading: Icon(Icons.password),
-                              title: TextFormField(
-                                initialValue: _accountList[index].password,
-                                obscureText: true,
-                                decoration:
-                                    InputDecoration(hintText: "Password"),
-                                onChanged: (value) {
-                                  setState(() {
-                                    _accountList[index].password = value;
-                                  });
-                                },
-                                validator: (value) {
-                                  if (value == null || value.isEmpty) {
-                                    return 'Password cannot be empty';
-                                  }
-                                },
-                              ),
-                            ),
-                          ),
-                        ),
-                        IconButton(
-                            onPressed: () {
-                              setState(() {
-                                print(
-                                    "Deleting: ${_accountList[index].username} - ${_accountList[index].password}");
-                                _accountList.removeAt(index);
-                              });
-                            },
-                            icon: Icon(Icons.delete))
-                      ],
-                    ),
-                  );
-                }),
-          ),
-          TextButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  addLoginData();
-                }
-              },
-              child: Text("Add"))
-        ],
-                ),
-              ),
+        ),
       ),
     );
   }
