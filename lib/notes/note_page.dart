2bd8782377521716b90766c63fb77bc96f07fbb8
Aswatth <aswatth56@gmail.com>
2024-10-08 14:56:50 -0400

    Update note_page.dart
    Display lock icon only if it is locked

--- lib/notes/note_page.dart
+++ lib/notes/note_page.dart
@@ -283,7 +283,7 @@ class _NotePageState extends State<NotePage> {
                                   ? Icon(
                                       Icons.lock,
                                     )
-                                  : Icon(Icons.lock_outline)
+                                  : Container()
                             ],
                           ),
                         ),
